3e05467e6cafad03b8f991c3d762d483d60e3dbc
PlasmaDrifter <118319137+PlasmaDrifter@users.noreply.github.com>
2026-09-24 22:55:01 -0700

    chore: release v0.3.1

--- server.py
+++ server.py
@@ -25,7 +25,7 @@ except ImportError:
 
 from scanner import scan_all_applications
 
-APP_VERSION = "0.3.0"
+APP_VERSION = "0.3.1"
 GITHUB_REPO = "PlasmaDrifter/AppIndex"
 
 app = FastAPI(title="AppIndex", version=APP_VERSION)
